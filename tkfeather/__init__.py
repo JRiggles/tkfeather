2a9b7de94810a84fda39f3fc4e33729edbff5311
John Riggles <jriggles@icloud.com>
2024-06-07 20:19:00 -0400

    add author, copyright, license info; imports

--- tkfeather/__init__.py
+++ tkfeather/__init__.py
@@ -1,1 +1,7 @@
+"""tkfeather - Feather icons for tkinter"""
 __all__ = ['tkfeather']
+__author__ = 'John Riggles <jriggles@icloud.com>'
+__copyright__ = '2024 John Riggles [sudo_whoami]'
+__license__ = 'MIT'
+
+from .tkfeather import Feather  # noqa
